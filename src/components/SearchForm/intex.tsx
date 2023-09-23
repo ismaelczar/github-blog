6d450810be6ee55cb02a0283cf84633e3779e437
ismaelczar <103972495+ismaelczar@users.noreply.github.com>
2023-09-23 08:08:23 -0300

    Aplicando validação no formulário

--- src/components/SearchForm/intex.tsx
+++ src/components/SearchForm/intex.tsx
@@ -1,13 +1,34 @@
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { SearchFormContainer, SearchFormHeader } from './styled'
 
+const SearchFormSchema = z.object({
+  query: z.string(),
+})
+
+type SearchFormType = z.infer<typeof SearchFormSchema>
+
 export function SearchForm() {
+  const { register, handleSubmit, reset } = useForm<SearchFormType>({
+    resolver: zodResolver(SearchFormSchema),
+  })
+
+  function QueryPost(data: SearchFormType) {
+    console.log(data.query)
+    reset()
+  }
   return (
-    <SearchFormContainer>
+    <SearchFormContainer onSubmit={handleSubmit(QueryPost)}>
       <SearchFormHeader>
         <strong>Publicações</strong>
         <span>6 publicações</span>
       </SearchFormHeader>
-      <input type="text" placeholder="Buscar Publicações" />
+      <input
+        type="text"
+        placeholder="Buscar Publicações"
+        {...register('query')}
+      />
     </SearchFormContainer>
   )
 }
